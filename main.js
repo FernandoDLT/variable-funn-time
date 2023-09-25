a6afaabe9a1dc4808b3a5cfd97fcb7ec9e95c927
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-09-24 18:30:29 -0700

    golf-yardage-tracker

--- main.js
+++ main.js
@@ -25,10 +25,10 @@
 
 
 //
-let num = 15
-num = num - 10
-// num -= 2 Short version
-console.log(num)
+// let num = 15
+// num = num - 10
+// // num -= 2 Short version
+// console.log(num)
 
 //
 
@@ -37,13 +37,95 @@ console.log(num)
 // alert(numFromInput)
 
 //
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 
-h1.addEventListener('click', addTwo)
+// h1.addEventListener('click', addTwo)
 
-function addTwo() {
-    let numFromInput = Number(document.querySelector('#danceDanceRevolution').value)
-    numFromInput += 25
+// function addTwo() {
+//     let numFromInput = Number(document.querySelector('#danceDanceRevolution').value)
+//     numFromInput += 25
 
-    console.log(num + numFromInput)
+//     console.log(num + numFromInput)
+// }
+
+// document.querySelector('h1').addEventListener('click', checkYards)
+// function checkYards() {
+//     let yards = Number(document.querySelector('input').value)
+//     if (yards < 10) {
+//         console.log('Use your Putter, you got this...')
+        
+//     } else if (yards > 10 || yards < 80) {
+//         console.log('use your 60 Degree Wedge')
+
+//     } else if (yards > 81 || yards < 110) {
+//         console.log('use your Sand Wedge')
+        
+//     } else if (yards < 120) {
+//         console.log('use your Pitching Wedge')
+        
+//     } else if (yards < 130) {
+//         console.log('use your 9 Iron')
+
+//     } else if (yards < 140) {
+//         console.log('use your 8 Iron')
+        
+//     } else if (yards < 150) {
+//         console.log('use your 7 Iron')
+        
+//     } else if (yards < 160) {
+//         console.log('use your 6 Iron')
+        
+//     } else if (yards < 170) {
+//         console.log('use your 5 Iron')
+        
+//     } else if (yards === 180) {
+//         console.log('use your 4 Iron')
+
+//     } else if (yards === 190) {
+//         console.log('use your 3 Iron')
+
+//     } else if (yards === 200) {
+//         console.log('Use your 5 Wood')
+
+//     } else if (yards > 250) {
+//         console.log('Use your 3 Wood')
+
+//     } else {
+//         console.log('Use BIG Momma!')
+//     }
+// }
+
+document.querySelector('h1').addEventListener('click', checkYards);
+
+function checkYards() {
+    let yards = Number(document.querySelector('input').value);
+    if (yards < 9) {
+        console.log('Use your Putter, you got this...');
+    } else if (yards >= 10 && yards < 79) {
+        console.log('Use your 60 Degree Wedge');
+    } else if (yards >= 80 && yards < 110) {
+        console.log('Use your Sand Wedge');
+    } else if (yards >= 110 && yards < 120) {
+        console.log('Use your Pitching Wedge');
+    } else if (yards >= 120 && yards < 130) {
+        console.log('Use your 9 Iron');
+    } else if (yards >= 130 && yards < 140) {
+        console.log('Use your 8 Iron');
+    } else if (yards >= 150 && yards < 160) {
+        console.log('Use your 7 Iron');
+    } else if (yards >= 160 && yards < 170) {
+        console.log('Use your 6 Iron');
+    } else if (yards >= 170 && yards < 180) {
+        console.log('Use your 5 Iron');
+    } else if (yards >= 180 && yards < 190) {
+        console.log('Use your 4 Iron');
+    } else if (yards >= 190 && yards < 200) {
+        console.log('Use your 3 Iron');
+    } else if (yards >= 200 && yards < 220) {
+        console.log('Use your 5 Wood');
+    } else if (yards >= 220 && yards < 250) {
+        console.log('Use your 3 Wood');
+    } else if (yards > 250) {
+        console.log('Use BIG Momma!');
+    }
 }
